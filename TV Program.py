322d897b5579672f630d2e7bf9b1c7c28bdfd9f4
unknown <morco.cathleenjan.12192003@gmail.com>
2023-05-13 16:26:50 +0800

    Assigning appropriate data types onto instances. Defining functions that were missing. Creating basic outline/logic of the program

--- TV Program.py
+++ TV Program.py
@@ -1,25 +1,44 @@
 class TV:
     def __init__(self):
-        self.channel
-        self.volumeLevel
-        self.on
+        self.channel = 1
+        self.volumeLevel = 1
+        self.on = False
 
     def turnOn(self):
-        self.on
+        self.on = True
 
     def turnOff(self):
-        self.on
+        self.on = False
 
-    def getChannel(self):
-        self.channel
+    def getChannel(self,channel):
+        return channel
 
-    def setChannel(self):
+    def setChannel(self,channel):
+        if self.on and 1 <= channel <= 120:
+            self.channel = channel
+   
+    def getChannel(self,volume):
+        return volume
+    
+    def setVolume(self,volume):
+        if self.on and 1 <= volume <= 7:
+            self.volume = volume
 
     def channelUp(self):
+        if self.on and self.channel < 120:
+            self.channel += 1
+            
+    def channelDown(self):
+        if self.on and self.channel > 1:
+            self.channel -= 1
 
-    def volumeUp(self):
-        
+    def volumeUp(self,volume):
+        if self.on and self.volume < 7:
+            self.volume += 1
+
     def volumeDown(self):
+        if self.on and self.volume > 1:
+            self.volume -= 1
 
 class TestTv:
     def main(self):
