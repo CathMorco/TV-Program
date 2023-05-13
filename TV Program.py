a9abd2f442e2f3ab4e430878c522db716a4a880a
unknown <morco.cathleenjan.12192003@gmail.com>
2023-05-13 16:08:44 +0800

    Listing the class, functions, variables, and methods from the UML Diagram

---
+++ TV Program.py
@@ -0,0 +1,19 @@
+class TV:
+    def __init__(self):
+        self.channel
+        self.volumeLevel
+        self.on
+
+TV()
+turnOn()
+turnOff()
+getChannel()
+setChannel(channel:)
+channelUp()
+channelDown()
+volumeUp()
+volumeDown()
+class TestTv:
+    def main(self):
+        tv1 = TV()
+        tv2 = TV()
