0ca868751689e0dd193b3665355e305075c187db
unknown <morco.cathleenjan.12192003@gmail.com>
2023-05-14 01:50:17 +0800

    Change most of the program. Replaced previous named variables with other names. Re-structured if-else statements

--- TestTV.py
+++ TestTV.py
@@ -1,6 +1,7 @@
+#Imports necessary elements
 from TVProgram import TV
-tv1 = TV()
-tv2 = TV()
+import sys
+
 
 class TestTv:
     def main(Channelnum1,Volumenum1,Channelnum2,Volumenum2):
@@ -20,35 +21,51 @@ class TestTv:
         choice= choice.lower()
         if choice[0]=="y":
             choice2=input("Would you like to increase or decrease the channel number?(Increase/Decrease): ")
-            choice2=choice2.lower()
-            if choice2[0]=="i":
-                choice3=input("Which tv's channel would you like to increase?(1/2): ")
-                choice3=choice3.lower()
-                if choice3[0]=="1":
-                    tv1.channelUp
+            choice2=choice2.lower()            
+            if choice2[0] == "i":
+                choice3 = input("Which tv's channel would you like to increase?(1/2): ")
+                choice3 = choice3.lower()
+                if choice3[0] == "1":
+                    tv1.channelUp()
                     print("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
-                elif choice3[0]=="2":
-                    tv2.channelUp
+                elif choice3[0] == "2":
+                    tv2.channelUp()
                     print("tv2's channel is", tv2.getChannel(), "and volume level is", tv2.getVolume())
                 else:
-                    SystemExit #Placeholder
-                    #Input Error Code
-            elif choice2[0]=="d":
-                choice3=input("Which tv's channel would you like to decrease?:(1/2)")
-                choice3=choice3.lower()
-                if choice3[0]=="1":
+                    print("Invalid input. Please try again.")
+                    TestTv.channelFunction()
+            elif choice2[0] == "d":
+                choice3 = input("Which tv's channel would you like to decrease?:(1/2)")
+                choice3 = choice3.lower()
+                if choice3[0] == "1":
                     tv1.channelDown()
                     print("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
-                elif choice3[0]=="2":
-                    tv2.channelDown
+                elif choice3[0] == "2":
+                    tv2.channelDown()
                     print("tv2's channel is", tv2.getChannel(), "and volume level is", tv2.getVolume())
                 else:
-                    SystemExit #Placeholder
-                    #Input Error Code
-        elif choice[0]=="n":
-            SystemExit
-            
+                    print("Invalid input. Please try again.")
+                    TestTv.channelFunction()
+            else:
+                print("Invalid input. Please try again.")
+                TestTv.channelFunction()
+        elif choice[0] == "n":
+            sys.exit()
+        again = input("Would you like to try again?(Y/N): ")
+        again = again.lower()
+        if again[0] == "y":
+            num1= int(input("From 1-120, what channel would you like for tv1?:"))
+            num2= int(input("From 1-7, what volume level would you like for tv1?:"))
+            num3= int(input("From 1-120, what channel would you like for tv2?:"))
+            num4= int(input("From 1-7, what volume level would you like for tv2?:"))
+            TestTv.main(num1,num2,num3,num4)
+            TestTv.channelFunction()
+        else:
+            sys.exit()
 
+
+tv1 = TV()
+tv2 = TV()
 num1= int(input("From 1-120, what channel would you like for tv1?:"))
 num2= int(input("From 1-7, what volume level would you like for tv1?:"))
 num3= int(input("From 1-120, what channel would you like for tv2?:"))
