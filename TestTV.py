3a0f61605bc10452a936bd7d30f87a4ce2c43d7e
unknown <morco.cathleenjan.12192003@gmail.com>
2023-05-13 23:58:20 +0800

    Made the program more interactable/customizable by setting parameters

--- TestTV.py
+++ TestTV.py
@@ -1,17 +1,17 @@
 from TVProgram import TV
 
 class TestTv:
-    def main():
+    def main(Channelnum1,Volumenum1,Channelnum2,Volumenum2):
         tv1 = TV()
         tv1.turnOn()
-        tv1.setChannel(30)
-        tv1.setVolume(3)
+        tv1.setChannel(Channelnum1)
+        tv1.setVolume(Volumenum1)
         print("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
         
         tv2 = TV()
         tv2.turnOn()
-        tv2.setChannel(3)
-        tv2.setVolume(2)
+        tv2.setChannel(Channelnum2)
+        tv2.setVolume(Volumenum2)
         print("tv2's channel is", tv2.getChannel(), "and volume level is", tv2.getVolume())      
 
-TestTv.main()
+TestTv.main(30,3,3,2)
